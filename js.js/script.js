e9daf7a72a3b21c83ea839233589234f32eb24bf
jayd <mushimirepacifique78@gmail.com>
2025-05-27 08:01:53 -0700

    pages

--- js.js/script.js
+++ js.js/script.js
@@ -17,3 +17,14 @@
         errorMessage.textContent = "Invalid email or password.";
       }
     });
+
+
+    
+  // Wait for page load
+  window.addEventListener("load", function () {
+    // Show loader for 2 seconds
+    setTimeout(function () {
+      document.getElementById("loader").style.display = "none";
+      document.getElementById("main-content").style.display = "block";
+    }, 2000); // 2000 milliseconds = 2 seconds
+  });
