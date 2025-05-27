2c372930de5db57629c46357f14b99b18b01ac67
jayd <mushimirepacifique78@gmail.com>
2025-05-27 08:01:53 -0700

    pages

--- js.js/home.js
+++ js.js/home.js
@@ -9,3 +9,4 @@
     }, 2000); // 2000 milliseconds = 2 seconds
   });
 
+
--- js.js/px.js
+++ js.js/px.js
@@ -17,3 +17,16 @@
         document.getElementById("message").textContent = "Please fill all fields.";
       }
     });
+
+
+
+    
+  // Wait for page load
+  window.addEventListener("load", function () {
+    // Show loader for 2 seconds
+    setTimeout(function () {
+      document.getElementById("loader").style.display = "none";
+      document.getElementById("main-content").style.display = "block";
+    }, 2000); // 2000 milliseconds = 2 seconds
+  });
+
